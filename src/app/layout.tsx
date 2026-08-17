816ccf21e6d4fd68b9e4e777c11b9c6ebbd77aaf
Matheus Santos <mateusfreiitas99@gmail.com>
2026-08-17 10:28:28 -0300

    adição de GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -128,29 +128,27 @@ export default function RootLayout({
           />
         ))}
 
-        {/* Google Tag Manager - placeholder for easy integration */}
-        {/* 
+        {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-XXXXXXX');
+          })(window,document,'script','dataLayer','GTM-N4ZQSPNR');
         `}} />
-        */}
+        {/* End Google Tag Manager */}
       </head>
       <body className="min-h-full flex flex-col">
-        {/* Google Tag Manager (noscript) - placeholder */}
-        {/*
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N4ZQSPNR"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        */}
+        {/* End Google Tag Manager (noscript) */}
 
         <Header />
         <main className="flex-1">{children}</main>
